c3bc2ecb6b6d2915d10ff89f277e5b1155362105
Jacob <78283092+Dunn-Dev8@users.noreply.github.com>
2022-12-03 01:58:12 +1030

    First Commit

---
+++ Apache/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo "Make sure Apache2 is installed Before Running"
+echo "sudo apt update"
+echo "sudo apt install apache2"
+sleep .5
+
+sudo cp ../index.html /var/www/html
+sudo cp ../Contact.html /var/www/html
+sudo cp -R ../posts/ /var/www/html
+sudo cp ../favicon.ico /var/www/html
+sudo cp ../postmkr.py /var/www/html
